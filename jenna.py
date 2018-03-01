42dad4baa70c879a0c25d7e6e4fdf2ade445bc66
Unknown <jf.riseley@gmail.com>
2018-03-01 18:06:06 +0000

    moved input files into repo

---
+++ jenna.py
@@ -0,0 +1,18 @@
+# A ride object
+
+
+class Ride:
+	def __init__(self, row_of_start, column_of_start, row_of_finish, column_of_finish, earliest_start, latest_finish):
+		self.a = row_of_start
+		self.b = column_of_start
+		self.x = row_of_finish
+		self.y = column_of_finish
+		self.s = earliest_start
+		self.f = latest_finish
+
+
+
+
+
+if __name__ == "__main__":
+	print("bleeee")
