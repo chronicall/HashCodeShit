7bbc266c6c1cd0740c59224d9455dc8c50d05f4f
Unknown <jf.riseley@gmail.com>
2018-03-01 19:31:14 +0000

    I am a waste of time

--- jenna.py
+++ jenna.py
@@ -46,16 +46,52 @@ class Car:
 	def __init__(self, ID):
 		self.ID = ID # the index of this car. From 1 to Simulation.F.
 		self.rides = [] # The simulation should populate this list with the rides that a car will take. 
-		self.column = 0
+
+		# ---STATE VARIABLES ----
 		self.row = 0
+		self.column = 0
 		self.simulation_time = 0
+		# ----------
 
+	# Return -1 if the ride is impossible.
+	# Otherwise, return the amount of time the car spends combined travelling to the ride AND waiting for ride to start.
 	def time_wasted_on_ride(self, ride):
+		# 1. Check whether ride is possible.
 
-	def reset(self)
-		column = 0
-		row = 0
-		simulatin_time = 0
+		# A ride is possible if (1) the driver can get there before ride.latest_start 
+		#
+		distance_to_ride_location = (abs(self.row-ride.row_of_start) - abs(self.column- ride.colum_of_start))
+		time_of_arrival_at_start = self.simulation_time + distance_to_ride_location
+
+		# Can the car get there before the ride's latest start time?
+		if time_of_arrival_at_start > ride.latest_start:
+			return -1 # ride is impossible 
+
+		# If we got here, the ride is possible. Now, calculate the following:
+		# How much time does the car waste DRIVING TO the start location?
+		# AND
+		# How much time does the car waste WAITING for the earliest_start?
+
+
+		if ride.earliest_start > time_of_arrival_at_start:
+			# Take into consideration time spent waiting before the car can depart.
+			time_spent_waiting_at_start = ride.earliest_start - time_of_arrival_at_start
+
+		time_wasted = distance_to_ride_location + time_spent_waiting_at_start
+
+		return time_wasted
+		
+		
+		
+
+		pass
+
+	def update(self, t, column, row):
+		self.simulation_time = self.simulation_time + t
+		self.column = column
+		self.row = row
+
+
 
 
 
@@ -85,8 +121,15 @@ if __name__ == "__main__":
     for ride in rides:
         print ride
 
+    # TODO DELET
+   
+
     # TODO: create a simulation object.
     simulation = Simulation(R, C, F, N, B, T)
+
+    d = simulation
+
+
 
     # TODO: Generate the car objects and store them in Simulation.cars. 
     # eg for i in range(1,Simulation.F+1):
@@ -98,4 +141,5 @@ if __name__ == "__main__":
 
     # TODO: Print our solution to file.
     #simulation.write_solution_to_file()
+
 
