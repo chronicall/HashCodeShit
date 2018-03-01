a63a38dfedb78f8c124757c1e60a7adfe1472d61
Sandra <entari@gmail.com>
2018-03-01 18:58:25 +0000

    Parse input files

--- jenna.py
+++ jenna.py
@@ -1,86 +1,90 @@
 # A ride object
 
+from sys import argv
+
+from parser import Parser
+
+
 class Simulation:
-	def __init__(self, R, C, F, N, B, T):
-		self.R = R # number of rows on the grid
-		self.C = C # number of columns on the grid
-		self.F = F # number of vehicles in the fleet
-		self.N = N # number of rides
-		self.B = B # per-ride bonus for starting the ride on time
-		self.cars = []
-		self.unassigned_rides = []
+    def __init__(self, R, C, F, N, B, T):
+        self.R = R # number of rows on the grid
+        self.C = C # number of columns on the grid
+        self.F = F # number of vehicles in the fleet
+        self.N = N # number of rides
+        self.B = B # per-ride bonus for starting the ride on time
+        self.cars = []
+        self.unassigned_rides = []
 
 
-	# TODO: write the solution to file.
-	# Do this by looping over each car in cars.
-	# car_ID_1 ride_id_0 ride_id_1 ... ride_id_n
-	# ...
-	# car_ID_F ride_id_0 ... ride_id_n
-	def write_solution_to_file(self):
-		print("TODO: implement write solution to file method of Simulation object")
-		pass
+        # TODO: write the solution to file.
+        # Do this by looping over each car in cars.
+        # car_ID_1 ride_id_0 ride_id_1 ... ride_id_n
+        # ...
+        # car_ID_F ride_id_0 ... ride_id_n
+        def write_solution_to_file(self):
+            print("TODO: implement write solution to file method of Simulation object")
+            pass
 
 
 class Ride:
-	def __init__(self, row_of_start, column_of_start, row_of_finish, column_of_finish, earliest_start, latest_finish, ID):
-		self.ID = ID # the ID of the ride, from 0 to N-1
-		self.a = row_of_start
-		self.b = column_of_start
-		self.x = row_of_finish
-		self.y = column_of_finish
-		self.s = earliest_start
-		self.f = latest_finish
-		self.distance = self.compute_distance()
-		self.latest_start = self.latest_finish - self.distance
+        def __init__(self, row_of_start, column_of_start, row_of_finish, column_of_finish, earliest_start, latest_finish, ID):
+            self.ID = ID # the ID of the ride, from 0 to N-1
+            self.a = row_of_start
+            self.b = column_of_start
+            self.x = row_of_finish
+            self.y = column_of_finish
+            self.s = earliest_start
+            self.f = latest_finish
+            self.distance = self.compute_distance()
+            self.latest_start = self.latest_finish - self.distance
 
-	# Compute the distance of this ride (manhattan distance)
-	def compute_distance(self):	
-		return (abs(this.a - this.x) + abs(this.b - this.y))
+        # Compute the distance of this ride (manhattan distance)
+        def compute_distance(self):	
+            return (abs(this.a - this.x) + abs(this.b - this.y))
 
 class Car:
-	def __init__(self, ID):
-		self.ID = ID # the index of this car. From 1 to Simulation.F.
-		self.rides = [] # The simulation should populate this list with the rides that a car will take. 
+    def __init__(self, ID):
+        self.ID = ID # the index of this car. From 1 to Simulation.F.
+        self.rides = [] # The simulation should populate this list with the rides that a car will take. 
 
 
 if __name__ == "__main__":
-	print(" ")
-	print("----------------------------")
-	print("Hashcode 2018 TEAM BING WHOO")
-	print("----------------------------")
-	print(" ")
+    print(" ")
+    print("----------------------------")
+    print("Hashcode 2018 TEAM BING WHOO")
+    print("----------------------------")
+    print(" ")
 
-	# TODO: Open the file. Assign variables R, C, F, N, B, T.
+    script, filename = argv
+    p = Parser(filename)
 
-	R = 0
-	C = 0
-	F = 0
-	N = 0
-	B = 0
-	T = 0
+    # TODO: Open the file. Assign variables R, C, F, N, B, T.
 
-	# Create a simulation object.
-	simulation = Simulation(R, C, F, N, B, T)
-	
+    R, C, F, N, B, T = p.parseSimulator()
+    print "Rows: %d" % R
+    print "Columns: %d" % C
+    print "Vehicles: %d" % F
+    print "Rides: %d" % N
+    print "Bonus: %d" % B
+    print "Time: %d" % T
+    print ""
 
-	# TODO: Generate the car objects and store them in Simulation.cars. 
-	# eg for i in range(1,Simulation.F+1):
-		# generate car object and append to simulation.cars
+    rides = p.parseRides()
 
+    for ride in rides:
+        print ride
 
-	# TODO: Generate the ride objects and store them in Simulation.rides.
-	# eg for i in range(0,simulation.N)
-		# generate a ride object and append to simulation.unassigned_rides
+    # TODO: create a simulation object.
+    simulation = Simulation(R, C, F, N, B, T)
 
+    # TODO: Generate the car objects and store them in Simulation.cars. 
+    # eg for i in range(1,Simulation.F+1):
+    # generate car object and append to simulation.cars
 
+    # TODO: COMPUTATION.
+    # SOLVE THE PROBLEM.
+    # ?????????????????????????????
 
+    # TODO: Print our solution to file.
+    #simulation.write_solution_to_file()
 
-	# TODO: COMPUTATION.
-	# SOLVE THE PROBLEM.
-	# ?????????????????????????????
-
-
-
-	# TODO: Print our solution to file.
-	simulation.write_solution_to_file()
-	
